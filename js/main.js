b41f9d5773e713664c3919762a6ef0478fe866b1
sindystar <hasungah@gmail.com>
2022-12-10 20:21:43 +0900

    탭 작업중

--- js/main.js
+++ js/main.js
@@ -1,50 +1,107 @@
-//btnCall
-const btnCall = document.querySelector(".btnCall");
-const menuMo = document.querySelector(".menuMo");
-btnCall.onclick = function(e){
+// slider 관련 js--------------------------
+const slider = document.querySelector("#slider");
+const ul = slider.querySelector("ul");
+const lis = ul.querySelectorAll("li");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let len = lis.length;
+
+// span
+const pg = document.querySelector(".pagination");
+const sp = pg.querySelector("span");
+const total = lis.length;
+sp.innerText = total;
+
+// strong --> css까지 완성하고 나서 다시 작성해보기
+// const str = pg.querySelector("strong");
+// str.innerText = 
+
+// slider
+let enableClick = true;
+let speed = 500;
+
+init();
+
+next.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (enableClick) {
+    nextslide();
+    enableClick = false;
+  }
+})
+prev.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (enableClick) {
+    prevslide();
+    enableClick = false;
+  }
+})
+
+function init() {
+  ul.style.left = "-100%";
+  ul.prepend(ul.lastElementChild);
+  ul.style.width = `${100 * len} %`;
+  lis.forEach((el) => {
+    el.style.width = `${100 / len}%`;
+  })
+}
+function nextslide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "-200%",
+    duration: speed,
+    callback: () => {
+      ul.style.left = "-100%";
+      ul.append(ul.firstElementChild);
+      enableClick = true;
+    }
+  })
+}
+function prevslide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "0%",
+    duration: speed,
+    callback: () => {
+      ul.style.left = "-100%";
+      ul.prepend(ul.lastElementChild);
+      enableClick = true;
+    }
+  })
+}
+
+// tab menu 관련 js--------------------------
+const container = document.querySelector(".container");
+const btns = container.querySelectorAll("ul li");
+const boxes = container.querySelectorAll("section article");
+// let enableClick = true;
+// let speed = 500;
+
+btns.forEach((_el, _ind) => {
+  _el.addEventListener("click", (e) => {
     e.preventDefault();
-    btnCall.classList.toggle("on");
-    menuMo.classList.toggle('on');
-}
 
+    let isOn = e.currentTarget.classList.contains("on");
+    if (isOn) return;
 
+    if (enableClick) {
+      enableClick = false;
 
+      activation(btns, _ind);
+      activation(boxes, _ind);
+    }
+  })
+})
 
-// main page basic slide
-let curPos = 0;
-let postion = 0;
-const IMAGE_WIDTH = 580;
-const prevBtn = document.querySelector(".prev")
-const nextBtn = document.querySelector(".next")
-const images = document.querySelector(".images")
- 
-function prev(){
-  if(curPos > 0){
-    nextBtn.removeAttribute("disabled")
-    postion += IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos - 1;
+function activation(list, index) {
+  for (let k of list) {
+    k.classList.remove("on");
+    list[index].classList.add("on");
+
+    setTimeout(() => {
+      enableClick = true;
+    }, speed)
+
   }
-  if(curPos == 0){
-    prevBtn.setAttribute('disabled', 'true')
-  }
+
 }
-function next(){
-  if(curPos < 4){
-    prevBtn.removeAttribute("disabled")
-    postion -= IMAGE_WIDTH;
-    images.style.transform = `translateX(${postion}px)`;
-    curPos = curPos + 1;
-  }
-  if(curPos == 4){
-    nextBtn.setAttribute('disabled', 'true')
-  }
-}
- 
-function init(){
-  prevBtn.setAttribute('disabled', 'true')
-  prevBtn.addEventListener("click", prev)
-  nextBtn.addEventListener("click", next)
-}
- 
-init(); 
